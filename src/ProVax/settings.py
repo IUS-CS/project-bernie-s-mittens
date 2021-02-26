6dba1b6fdeae77848ff92716eeae50bd161aea62
Amanda Schneider <48770305+SchneiderAJ@users.noreply.github.com>
2021-02-26 18:58:25 -0500

    Created admin page and superusers

--- src/ProVax/settings.py
+++ src/ProVax/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'helloWorld.apps.HelloworldConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
